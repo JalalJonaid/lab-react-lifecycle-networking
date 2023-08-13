bf706c1b47749679e1c0a3510a79a33a3516e84d
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-08-13 12:19:19 -0400

    Lab w/ Davon: React Lifecycle

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,10 +1,29 @@
+import {React, useEffect, useState} from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
 export const Employee = () => {
+
+  const [employees, setEmployees] = useState([]);
+
+  function getFeaturedEmployees() {
+    fetch("https://one0-2-vet-api.onrender.com/api/employees")
+      .then((data) => (data.json()))
+      .then((json)=> {
+        setEmployees(json)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getFeaturedEmployees();
+  }, []);
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
+      <h3>Test {employees.length ? employees[0].firstName : ""}</h3>
       <h4>Staff Member Title</h4>
       <button>Show Pets</button>
       <PetList />
